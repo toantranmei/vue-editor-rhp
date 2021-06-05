442b9fbfd9a25aa9e387e4577c9fec862174ff8e
Sky Albert <trantoan.fox.97@gmail.com>
2021-06-05 16:34:06 +0700

    fix(plugin): change config to package version

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,7 @@
-const version = "__VERSION__";
 import EditorComponent from "../Editor.vue";
 
-export function install(app) {
+export default function install(app) {
   if (install.installed) return;
   install.installed = true;
   app.component("Editor", EditorComponent);
 }
-
-const plugin = {
-  install,
-  version,
-};
-
-export default plugin;
