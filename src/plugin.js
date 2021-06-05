2b41fe0e1b4771a5075f491a0cd94f9281a8ffbc
Tran Toan <trantoan.fox.97@gmail.com>
2021-06-05 17:39:12 +0700

    fix(plugin): add auto config plugin outside plugins

--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,29 @@
-import EditorComponent from "../Editor.vue";
+const version = "__VERSION__";
+import EditorComponent from "./Editor.vue";
 
-export default function install(app) {
+export function install(Vue) {
   if (install.installed) return;
   install.installed = true;
-  app.component("Editor", EditorComponent);
+  Vue.component("Editor", EditorComponent);
 }
+
+const plugin = {
+  install,
+  version,
+};
+
+export const Editor = EditorComponent;
+
+let GlobalVue = null;
+
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+export default plugin;
